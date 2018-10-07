a7a4a60c00d87f493319cd7f7bdc5d8f5db8b9b1
Tom Harrop <twharrop@gmail.com>
2018-10-08 10:15:32 +1300

    sort vst by variance

--- src/deseq_qc.R
+++ src/deseq_qc.R
@@ -11,5 +11,19 @@ dds <- estimateSizeFactors(dds)
 # transform read counts
 vst <- varianceStabilizingTransformation(dds)   # use blind = FALSE for QC
 
+# get transformed counts
+vst_assay <- assay(vst)
+vst_vars <- rowVars(vst_assay)
+vst_sorted <- vst_assay[order(vst_vars),]
+
+# write the sorted VST table
+write.csv(vst_sorted, "output/deseq2/vst_sorted.csv")
+
+# does this make sense?
+dds$timepoint <- factor(dds$timepoint)
+plotCounts(dds, "FBgn0034519", "timepoint")
+
 # plot deseq's PCA
 plotPCA(vst, intgroup = c("timepoint", "treatment"), ntop = Inf)
+
+
